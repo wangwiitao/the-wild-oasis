03c6cac29d27843d101334e2a67e93ceae34fdac
975569220@qq.com <975569220@qq.com>
2025-12-17 23:57:07 +0800

    start

---
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -0,0 +1,120 @@
+import styled from "styled-components";
+import BookingDataBox from "../../features/bookings/BookingDataBox";
+
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
+import ButtonText from "../../ui/ButtonText";
+
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "../bookings/useBooking";
+import Spinner from "../../ui/Spinner";
+import { useEffect, useState } from "react";
+import Checkbox from "../../ui/Checkbox";
+import { formatCurrency } from "../../utils/helpers";
+import { useChecking } from "./useChecking";
+import { useSettings } from "../settings/useSettings";
+
+const Box = styled.div`
+  /* Box */
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+  padding: 2.4rem 4rem;
+`;
+
+function CheckinBooking() {
+  const moveBack = useMoveBack();
+  const { booking, isLoading } = useBooking();
+  const [confirmPaid, setConfirmPaid] = useState();
+  const { checkin, isCheckingIn } = useChecking();
+  const [addBreakfast, setAddBreakfast] = useState(false);
+  const { settings, isLoading: isLoadingSetting } = useSettings();
+  const {
+    id: bookingId,
+    guests,
+    totalPrice,
+    numGuests,
+    hasBreakfast,
+    numNights,
+  } = booking;
+  const { breakfastPrice } = settings;
+  const optionalBreakfastPrice = breakfastPrice * numNights * numGuests;
+
+  function handleCheckin() {
+    if (!confirmPaid) return;
+
+    if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
+    } else {
+      checkin({ bookingId, breakfast: {} });
+    }
+  }
+
+  useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking]);
+  if (isLoading || isLoadingSetting) return <Spinner />;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Check in booking #{bookingId}</Heading>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+      </Row>
+
+      <BookingDataBox booking={booking} />
+
+      {!hasBreakfast && (
+        <Box>
+          <Checkbox
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((add) => !add);
+              setConfirmPaid(false);
+            }}
+            id="brekfast"
+          >
+            Want to add breakfast for {formatCurrency(optionalBreakfastPrice)} ?
+          </Checkbox>
+        </Box>
+      )}
+
+      <Box>
+        <Checkbox
+          checked={confirmPaid}
+          onChange={() => setConfirmPaid((confirm) => !confirm)}
+          id={"confirm"}
+          disabled={confirmPaid || isCheckingIn}
+        >
+          I confirm that {guests.fullName} has paid the total amount of
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                totalPrice + optionalBreakfastPrice
+              )} (${formatCurrency(totalPrice)}+${formatCurrency(
+                optionalBreakfastPrice
+              )})`}
+        </Checkbox>
+      </Box>
+
+      <ButtonGroup>
+        <Button onClick={handleCheckin} disabled={!confirmPaid || isCheckingIn}>
+          Check in booking #{bookingId}
+        </Button>
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+export default CheckinBooking;
+
+// There was an error while checking in
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+// const test = css`
+//   text-align: center;
+//   ${10 > 5 && "background-color:var(--color-green-700);"}
+// `;
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    `
+     font-size: 3rem;
+     font-weight: 600;
+`}
+  ${(props) =>
+    props.as === "h2" &&
+    `
+     font-size: 2rem;
+     font-weight: 600;
+`}
+  ${(props) =>
+    props.as === "h3" &&
+    `
+     font-size: 2rem;
+     font-weight: 500;
+`}
+ ${(props) =>
+    props.as === "h4" &&
+    `
+     font-size: 3rem;
+     font-weight: 600;
+     text-align:center;
+`}
+
+  line-height:1.4;
+`;
+
+export default Heading;
